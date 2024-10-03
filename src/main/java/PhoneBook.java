44f4eb82f93fb4e653959d3f5bf3af5108a91ad1
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-10-03 10:41:08 +0700

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,14 @@
-public class PhoneBook {
+import java.util.Map;
+import java.util.TreeMap;
 
+public class PhoneBook {
+    private final Map<String, String> CONTACTS;
+
+    public PhoneBook() {
+        CONTACTS = new TreeMap<>();
+    }
+
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,49 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
 
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    void addTest() {
+
+        int resultCount;
+        int expectedCount = 0;
+        final int CHECK_NAME = -1;
+
+        try {
+            resultCount = phoneBook.add("testName", "1234567890");
+            expectedCount = expectedCount + 1;
+
+            assertEquals(expectedCount, resultCount);
+        } catch (Exception t1) {
+            System.out.println("testAddFirst_err");
+        }
+
+        try {
+            resultCount = phoneBook.add("newTestName", "1234567890");
+            expectedCount = expectedCount + 1;
+
+            assertEquals(expectedCount, resultCount);
+        } catch (Exception t2) {
+            System.out.println("testAddNext_err");
+        }
+
+        try {
+            int testEntryName = phoneBook.add("testName", "1234567890");
+
+            assertEquals(CHECK_NAME, testEntryName);
+        } catch (Exception t3) {
+            System.out.println("testName_err");
+        }
+
+        try {
+            phoneBook.add("testNumberName", "invalidNumber");
+        } catch (Exception e) {
+            assertEquals("invalid number", e.getMessage());
+        }
+
+    }
 }
